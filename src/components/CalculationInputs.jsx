f300848b15a0f9c40fab1af3719a4f7a94c6185f
deivydas.kim <deivydas.kim@gmail.com>
2024-06-14 11:25:15 +0300

    create calculation inputs

--- src/components/CalculationInputs.jsx
+++ src/components/CalculationInputs.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-  const initialInputs = {
-    initialInvestment: 15000,
-    annualInvestment: 900,
-    expectedReturn: 5,
-    duration: 12
-  }
+const initialInputs = {
+  initialInvestment: 15000,
+  annualInvestment: 900,
+  expectedReturn: 5,
+  duration: 12,
+};
 
 export default function CalculationInputs() {
-  const [userInputs, setUserInputs] = useState(initialInputs)
+  const [userInputs, setUserInputs] = useState(initialInputs);
 
-  function changeInputHandler() {
-    
+  function changeInputHandler(e) {
+    const { name, value } = e.target;
+    setUserInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: value,
+    }));
   }
 
   return (
@@ -19,21 +23,41 @@ export default function CalculationInputs() {
       <div className="input-group">
         <label htmlFor="">
           Initial Investment
-          <input type="number" value={userInputs.initialInvestment} />
+          <input
+            type="number"
+            name="initialInvestment"
+            onChange={changeInputHandler}
+            value={userInputs.initialInvestment}
+          />
         </label>
         <label htmlFor="">
           Annual Investment
-          <input type="number" value={userInputs.annualInvestment}/>
+          <input
+            type="number"
+            name="annualInvestment"
+            onChange={changeInputHandler}
+            value={userInputs.annualInvestment}
+          />
         </label>
       </div>
       <div className="input-group">
         <label htmlFor="">
           Expected Return
-          <input type="number" value={userInputs.expectedReturn}/>
+          <input
+            type="number"
+            name="expectedReturn"
+            onChange={changeInputHandler}
+            value={userInputs.expectedReturn}
+          />
         </label>
         <label htmlFor="">
           Duration
-          <input type="number" value={userInputs.duration}/>
+          <input
+            type="number"
+            name="duration"
+            onChange={changeInputHandler}
+            value={userInputs.duration}
+          />
         </label>
       </div>
     </section>
